c56cf1d77a8027e56a4ccc5e224874ca95ad21ca
mohdt-optimus <mohd.taha@optimusinfo.com>
2015-08-08 18:37:39 +0530

    Minor error removed

---
+++ FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  twitterFlickr_swift
+//
+//  Created by optimusmac-12 on 08/08/15.
+//  Copyright (c) 2015 mdtaha.optimus. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
